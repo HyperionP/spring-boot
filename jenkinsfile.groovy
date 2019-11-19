331cf9c6e4334dbe4118a058b7b75323f1cde0c3
HyperionP <32985195+HyperionP@users.noreply.github.com>
2019-11-19 19:17:38 +0200

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,1 +1,22 @@
-
+node(){
+    def workDir = sh(returnStdout: true, script: "pwd").trim()
+    stage('CHEKOUT'){ 
+        //sh"sudo chmod -R 777 ."
+        deleteDir()
+        
+        // withCredentials([usernamePassword(credentialsId: '0765f4d3-40d9-4d02-a696-5891d171ac31', passwordVariable: 'password', usernameVariable: 'name')]){
+            // sh"git clone https://$name:$password@github.com/HyperionP/spring-boot.git"
+        // }
+        
+    }
+    stage('BUILD'){
+    //sh "cd $workDir && cd ./spring-boot/spring-boot-project/spring-boot && ls -lh && sudo mvn clean install"
+    }
+}
+//input("Approve to continue")
+node(){
+     stage('save'){
+       // archiveArtifacts artifacts: "**/target/*.jar", fingerprint: true
+    }
+    
+}
