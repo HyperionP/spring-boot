b9772d9542d80b42e983aa5e83cc53b6977d61f8
HyperionP <32985195+HyperionP@users.noreply.github.com>
2019-11-19 20:06:03 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -22,7 +22,7 @@ node(){
     stage('CHEKOUT'){ 
         //sh"sudo chmod -R 777 ."
         deleteDir()
-        
+        checkout scm
         // withCredentials([usernamePassword(credentialsId: '0765f4d3-40d9-4d02-a696-5891d171ac31', passwordVariable: 'password', usernameVariable: 'name')]){
             // sh"git clone https://$name:$password@github.com/HyperionP/spring-boot.git"
         // }
