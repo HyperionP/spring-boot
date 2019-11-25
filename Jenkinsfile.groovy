fcd445490ed65ef101c6bb1b0d1dfa08fb4f2472
HyperionP <32985195+HyperionP@users.noreply.github.com>
2019-11-25 13:45:27 +0200

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -32,34 +32,37 @@ properties([
         )
     ])
 ])
+def server = Artifactory.newServer url: 'http://35.228.43.230:8081/artifactory', credentialsId: '9b898bd5-1092-42b9-a0c2-1b0dc9ca9bb2'
 node(){
+    
     def workDir = sh(returnStdout: true, script: "pwd").trim()
     stage('CHEKOUT'){ 
-		currentBuild.displayName = "#${BUILD_NUMBER} text1 ${params.BRANCH}"
-        //sh"sudo chmod -R 777 ."
-		ansiColor('xterm') {
-            printlnGreen "ttexttt"
-        }
+        sh"sudo chmod -R 777 ."
         deleteDir()
-        checkout scm
-		
-		sh"pwd && ls -lh"
-        // withCredentials([usernamePassword(credentialsId: '0765f4d3-40d9-4d02-a696-5891d171ac31', passwordVariable: 'password', usernameVariable: 'name')]){
-            // sh"git clone https://$name:$password@github.com/HyperionP/spring-boot.git"
-        // }
+        withCredentials([usernamePassword(credentialsId: 'aa0815bb-af35-4940-a3a8-b763ab24c501', passwordVariable: 'password', usernameVariable: 'name')]){
+            sh"git clone https://$name:$password@github.com/HyperionP/spring-boot.git"
+        }
         
     }
     stage('BUILD'){
-    //sh "cd $workDir && cd ./spring-boot/spring-boot-project/spring-boot && ls -lh && sudo mvn clean install"
+    sh "cd $workDir && cd ./spring-boot/spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-ui/ && ls -lh && sudo mvn clean install"
     }
-}
-//input("Approve to continue")
-node(){
-     stage('save'){
-       // archiveArtifacts artifacts: "**/target/*.jar", fingerprint: true
+    //input("Approve to continue")
+     stage('UPLOAD ARTIFACT'){
+         sh" cd $workDir && cd ./spring-boot/spring-boot-tests/spring-boot-smoke-tests/spring-boot-smoke-test-web-ui/target && sudo cp spring-boot-smoke-test-web-ui-2.2.0.BUILD-SNAPSHOT.jar ./spring-boot.latest.jar && sudo mv spring-boot-smoke-test-web-ui-2.2.0.BUILD-SNAPSHOT.jar ./spring-boot.${BUILD_NUMBER}.jar" 
+        archiveArtifacts artifacts: "**/target/*.jar", fingerprint: true
+        def uploadSpec = """{
+  "files": [
+    {
+      "pattern": "**/target/*.jar",
+      "target": "generic-local/"
     }
+ ]
+}"""
+server.upload spec: uploadSpec
+//ansiblePlaybook colorized: true, playbook: '$workDir/spring-boot/deploy_artf.yml'
+sh"sudo -S su - parasitchmax -c 'ansible-playbook deploy_artf.yml'"
+         
+     }
     
-}
-def printlnGreen(text) {
-    println "\033[1;4;37;42m$text\033[0m"
 }
